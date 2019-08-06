50ce8acf360484688b54adc4001e2207a827b42f
zssjz <zssjz@126.com>
2019-08-06 14:40:25 +0800

    delete

--- src/main/java/com/jason/components/dao/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jason.components.dao;
-
-import com.jason.components.model.RegionDO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<RegionDO, Integer> {
-}
